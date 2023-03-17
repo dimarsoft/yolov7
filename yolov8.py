dc7f368a951eece2fdd8d8ab9c5076c95c085c8a
Дмитрий Марандич <dimar_soft@hotmail.com>
2023-03-17 11:33:08 +0300

    conf

--- yolov8.py
+++ yolov8.py
@@ -32,7 +32,7 @@ def create_video_with_track(results, source_video, output_file):
     input_video.release()
 
 
-def run_single_video_yolo8(model, source, tracker, output_folder, save_vid=False, save_vid2=False):
+def run_single_video_yolo8(model, source, tracker, output_folder, conf=0.3, save_vid=False, save_vid2=False):
     print(f"start {source}")
     model = YOLO(model)
 
@@ -40,7 +40,7 @@ def run_single_video_yolo8(model, source, tracker, output_folder, save_vid=False
         source=source,
         stream=False,
         save=save_vid,
-        conf=0.0,
+        conf=conf,
         tracker=tracker
     )
     source_path = Path(source)
@@ -48,19 +48,20 @@ def run_single_video_yolo8(model, source, tracker, output_folder, save_vid=False
 
     print(f"save to: {text_path}")
 
-    yolo8_save_tracks_to_txt(results=track, txt_path=text_path, conf=0.0)
+    yolo8_save_tracks_to_txt(results=track, txt_path=text_path, conf=conf)
 
     if save_vid2:
         out_video_path = Path(output_folder) / f"{source_path.stem}.mp4"
         create_video_with_track(results=track, source_video=source, output_file=out_video_path)
 
 
-def run_yolo8(model, source, tracker, output_folder, save_vid=False, save_vid2=False):
+def run_yolo8(model, source, tracker, output_folder,  conf=0.3, save_vid=False, save_vid2=False):
     """
 
     Args:
+        conf: conf для трекера
         save_vid2: Создаем наше видео с центром человека
-        save_vid: save для model.track. Yolo создает свое видео
+        save_vid (Bool): save для model.track. Yolo создает свое видео
         output_folder: путь к папке для результатов работы, txt
         tracker: трекер (botsort.yaml, bytetrack.yaml) или путь к своему файлу с настройками
         source: путь к видео, если папка, то для каждого видеофайла запустит
@@ -74,6 +75,6 @@ def run_yolo8(model, source, tracker, output_folder, save_vid=False, save_vid2=F
         for entry in source_path.iterdir():
             # check if it is a file
             if entry.is_file() and entry.suffix == ".mp4":
-                run_single_video_yolo8(model, str(entry), tracker, output_folder, save_vid, save_vid2)
+                run_single_video_yolo8(model, str(entry), tracker, output_folder, conf, save_vid, save_vid2)
     else:
-        run_single_video_yolo8(model, source, tracker, output_folder, save_vid, save_vid2)
+        run_single_video_yolo8(model, source, tracker, output_folder, conf, save_vid, save_vid2)
