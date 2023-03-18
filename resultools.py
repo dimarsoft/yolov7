a7655df2faeaf342fbe3e8f5fdb81928d4dbf73c
Дмитрий Марандич <dimar_soft@hotmail.com>
2023-03-18 23:09:26 +0300

    results

--- resultools.py
+++ resultools.py
@@ -57,4 +57,3 @@ class TestResults:
         result_json_file = Path(output_folder) / "current_all_track_results.json"
         with open(result_json_file, "w") as write_file:
             write_file.write(json.dumps(self.result_items, indent=4, sort_keys=True, default=lambda o: o.__dict__))
-
