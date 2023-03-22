3878425e12a1870175b2d7a479b0946ad5154d2a
Дмитрий Марандич <dimar_soft@hotmail.com>
2023-03-22 21:09:43 +0300

    to int

--- count_results.py
+++ count_results.py
@@ -1,8 +1,8 @@
 class Deviation(object):
     def __init__(self, start, end, status):
-        self.start_frame = start
-        self.end_frame = end
-        self.status_id = status
+        self.start_frame = int(start)
+        self.end_frame = int(end)
+        self.status_id = int(status)
 
     def __str__(self):
         return f"{self.status_id}: [{self.start_frame}, {self.end_frame}]"
@@ -11,10 +11,11 @@ class Deviation(object):
 class Result:
     def __init__(self, humans, c_in, c_out, deviations):
         self.file = ""
-        self.humans = humans
-        self.counter_in = c_in
-        self.counter_out = c_out
+        self.humans = int(humans)
+        self.counter_in = int(c_in)
+        self.counter_out = int(c_out)
         self.deviations = deviations
 
     def __str__(self):
-        return f"file = {self.file}, in = {self.counter_in}, out = {self.counter_out}, deviations = {len(self.deviations)}"
+        return f"file = {self.file}, in = {self.counter_in}, " \
+               f"out = {self.counter_out}, deviations = {len(self.deviations)}"
