e8c4f28afd3bd568003f5deaadb8be0aa9f5a9a1
Дмитрий Марандич <dimar_soft@hotmail.com>
2023-03-25 22:49:46 +0300

    Sort.update(dets, img) нужно два параметра

--- trackers/sort/sort.py
+++ trackers/sort/sort.py
@@ -216,7 +216,7 @@ class Sort(object):
         self.trackers = []
         self.frame_count = 0
 
-    def update(self, dets):
+    def update(self, dets, _):
         """
     Params:
       dets - a numpy array of detections in the format [[x1,y1,x2,y2,score],[x1,y1,x2,y2,score],...]
