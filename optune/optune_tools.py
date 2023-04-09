25bc985c2b041e3f5213150dedc1463675988c48
Дмитрий Марандич <dimar_soft@hotmail.com>
2023-04-09 09:37:52 +0300

    дата в имени файла

--- optune/optune_tools.py
+++ optune/optune_tools.py
@@ -1,5 +1,6 @@
 import json
 import os
+from datetime import datetime
 from pathlib import Path
 
 import numpy as np
@@ -13,10 +14,11 @@ def reset_seed(seed=123):
     os.environ['PYTHONHASHSEED'] = str(seed)
 
 
-def save_result(trial: FrozenTrial, save_folder: str, tag: str) -> None:
+def save_result(trial: FrozenTrial, save_folder: str, tag: str, use_date: bool = True) -> None:
     """
     Сохраняет результаты работы Optune
     Args:
+        use_date(bool): Дополнить имя файла датой
         tag: Тип трекера, который использовали. Будет добавлено к имени файла
         trial(FrozenTrial): результат работы Optune
         save_folder: Папка, в которой будут сохранены результаты: value, param
@@ -24,6 +26,11 @@ def save_result(trial: FrozenTrial, save_folder: str, tag: str) -> None:
     Returns:
 
     """
+
+    if use_date:
+        now = datetime.now()
+        tag = f"{now.year:04d}_{now.month:02d}_{now.day:02d}_{now.hour:02d}_{now.minute:02d}_" \
+              f"{now.second:02d}_{tag}"
 
     value_json_file = Path(save_folder) / f"{tag}_value.json"
 
