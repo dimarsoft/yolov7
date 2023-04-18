41e3f7afd00621b1eca2143e11211892823e017e
Дмитрий Марандич <dimar_soft@hotmail.com>
2023-04-18 22:20:30 +0300

    функции для работы с видео файлами

---
+++ video_tools.py
@@ -0,0 +1,88 @@
+from pathlib import Path
+from typing import Union
+
+import cv2
+
+from configs import load_bound_line, CAMERAS_PATH, get_bound_line
+from post_processing.timur import get_camera
+
+
+def save_frames(path_to_video: Union[str, Path], output_folder: Union[str, Path], max_frames: int = -1):
+    input_video = cv2.VideoCapture(str(path_to_video))
+
+    fps = int(input_video.get(cv2.CAP_PROP_FPS))
+    # ширина
+    w = int(input_video.get(cv2.CAP_PROP_FRAME_WIDTH))
+    # высота
+    h = int(input_video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    # количество кадров в видео
+    frames_in_video = int(input_video.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    path_to_video = Path(path_to_video)
+
+    if max_frames > 0:
+        max_frames = min(max_frames, frames_in_video)
+    else:
+        max_frames = frames_in_video
+
+    output_folder = Path(output_folder)
+
+    for frame_id in range(max_frames):
+        ret, frame = input_video.read()
+        image_path = output_folder / f"{path_to_video.stem}_frame_{frame_id}_{w}_{h}.jpg"
+        cv2.imwrite(str(image_path), frame)
+
+    input_video.release()
+
+
+def draw_turnic_on_frame(frame, bound_line):
+    # турникет рисуем один раз
+    p1 = bound_line[0]
+    p2 = bound_line[1]
+    y1 = int(p1[1])
+    y2 = int(p2[1])
+    cv2.line(frame, (p1[0], y1), (p2[0], y2), (0, 255, 0), 2)
+
+
+def create_video(source_video, output_folder, bound_line):
+    input_video = cv2.VideoCapture(str(source_video))
+
+    fps = int(input_video.get(cv2.CAP_PROP_FPS))
+    # ширина
+    w = int(input_video.get(cv2.CAP_PROP_FRAME_WIDTH))
+    # высота
+    h = int(input_video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    # количество кадров в видео
+    frames_in_video = int(input_video.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    output_video_path = str(Path(output_folder) / Path(source_video).name)
+    output_video = cv2.VideoWriter(
+        output_video_path, cv2.VideoWriter_fourcc(*'mp4v'),
+        fps, (w, h))
+
+    for frame_id in range(frames_in_video):
+        ret, frame = input_video.read()
+        draw_turnic_on_frame(frame, bound_line)
+        output_video.write(frame)
+
+        print(f"{frame_id}/{frames_in_video}")
+
+    input_video.release()
+    output_video.release()
+
+
+def draw_turn(source, out_folder):
+    bound_lines = load_bound_line(CAMERAS_PATH)
+    camera_num, w, h, fps = get_camera(source)
+    bound_line = get_bound_line(bound_lines, camera_num)
+
+    create_video(source, out_folder, bound_line)
+
+
+if __name__ == '__main__':
+    # save_frames("D:\\48.mp4", "d:\\AI\\2023\\18.04.2023\\", 10)
+    # draw_turn("D:\\48.mp4", "d:\\AI\\2023\\18.04.2023\\")
+    draw_turn("D:\\44.mp4", "d:\\AI\\2023\\18.04.2023\\")
+    draw_turn("D:\\10.mp4", "d:\\AI\\2023\\18.04.2023\\")
